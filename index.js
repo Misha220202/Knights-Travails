1a899376e1500bc3ec21be64f98b17e728c2e54c
Misha220202 <157743520+Misha220202@users.noreply.github.com>
2024-08-27 16:46:22 -0600

    finished

---
+++ index.js
@@ -0,0 +1,39 @@
+const knightMoves = (start, end) => {
+
+    const moves = [[1, 2], [2, 1], [2, -1], [1, -2], [-1, -2], [-2, -1], [-2, 1], [-1, 2]];
+
+    const isValid = ([x, y]) => {
+        return x >= 0 && x < 8 && y >= 0 && y < 8;
+    }
+
+    const bfs = (start, end) => {
+        const queue = [[start]];
+        const visitedNodes = new Set();
+        visitedNodes.add(start.toString());
+
+        while (queue.length) {
+            const path = queue.shift();
+            const [x, y] = path[path.length - 1];
+            if (x == end[0] && y == end[1]) {
+                return path;
+            }
+
+            moves.forEach(move => {
+                const dx = move[0];
+                const dy = move[1];
+                const nextPosition = [x + dx, y + dy];
+                if (isValid(nextPosition) && !visitedNodes.has(nextPosition.toString())) {
+                    visitedNodes.add(nextPosition.toString());
+                    queue.push([...path, nextPosition]);
+                }
+            })
+        }
+
+        return null;
+    }
+    
+    return bfs(start, end);
+}
+
+const path = knightMoves([3, 3], [4, 3]);
+console.log(path);
